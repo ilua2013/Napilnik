6eecdc2fbd818e0592ca3a3c6ee1a6bb932be1fa
Илья <lite18340509@gmail.com>
2022-01-13 14:49:44 +0300

    .

--- Class1.cs
+++ Class1.cs
@@ -6,46 +6,28 @@ namespace Napilnik
 {
     class Weapon
     {
-        public int Damage
-        {
-            get { return Damage; }
-            private set
-            {
-                if (value < 0)
-                    throw new InvalidOperationException();
-                else
-                    Damage = value;
-            }
-        }
-        public int Bullets { get; private set; }
+        public int Damage;
+        public int Bullets;
 
         public void Fire(Player player)
         {
-            if (Bullets <= 0)
-                return;
-
-            player.ApplyDamage(this);
+            player.Health -= Damage;
             Bullets -= 1;
         }
     }
 
     class Player
     {
-        public int Health { get; private set; }
-
-        public void ApplyDamage(Weapon weapon)
-        {
-            Health -= weapon.Damage;
-        }
+        public int Health;
     }
 
     class Bot
     {
-        private Weapon _weapon;
+        public Weapon Weapon;
 
         public void OnSeePlayer(Player player)
         {
-            _weapon.Fire(player);
+            Weapon.Fire(player);
         }
     }
 }
